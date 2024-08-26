cf957dc5a694e53bb8c61c35e6d2df7dad72091a
DH <adlandh@gmail.com>
2024-08-26 09:02:24 +0200

    update deps

--- middleware.go
+++ middleware.go
@@ -17,7 +17,7 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
